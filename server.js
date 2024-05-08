03d485a7e2a30ec4dc28b26522de83d2cae75e2b
cook226 <112456726+cook226@users.noreply.github.com>
2024-05-08 04:04:23 -0400

    updated files

--- server.js
+++ server.js
@@ -1,21 +1,48 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const connectDB = require('./config/dbConnect');
 const statesRoutes = require('./routes/statesRoutes');
 
 // Connect to Database
 connectDB();
 
+const allowedOrigins = [
+  'https://your-app-name.onrender.com',
+  // Add any other front-end origins you have
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
 const app = express();
 
-// Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
+
+// Serve static files
+app.use(express.static('public'));
 
 // Routes
 app.use('/states', statesRoutes);
 
-// Start Server
-const PORT = process.env.PORT || 5000;
+// Root endpoint
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// 404 Error handler middleware
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
